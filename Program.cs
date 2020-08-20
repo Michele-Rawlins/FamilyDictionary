b7bbaa75a33822ae8f6f0c437d77640390376489
Michele Rawlins <shellbop@att.net>
2020-08-19 20:33:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { {"name" ,"Rachel" }, {"age", "34" } });
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Lois" }, { "age", "77" } });
+            myFamily.Add("oldest-brother", new Dictionary<string, string>() { { "name", "Chuck" }, { "age", "49" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Russell" }, { "age", "41" } });
+            myFamily.Add("youngest-brother", new Dictionary<string, string>() { { "name", "Nathan" }, { "age", "36" } });
+            //myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Jeremy" }, { "age", "38" } });
+          
+
+
+            foreach (var familyMember in myFamily )
+            {
+                            
+                    Console.WriteLine($"{familyMember.Value["name"]} is my {familyMember.Key} and is {familyMember.Value["age"]} years old.");
+               
+            }
+        }
+    }
+}
